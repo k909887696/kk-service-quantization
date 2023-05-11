c87d39fedf7b97747aa8db6042041f8e867b422a
k909887696 <909887696@qq.com>
2023-05-11 23:35:37 +0800

    1

--- kk-api-quantization/start.bat
+++ kk-api-quantization/start.bat
@@ -1,3 +1,6 @@
 echo start
+E:
+cd E:\deploy\kk-api-quantization
 start javaw -Xms256m -Xmx1G -Xmn256m -jar -Dfile.encoding=utf-8 %1 --spring.profiles.active=%2 --spring.cloud.nacos.config.server-addr=%3 &
+echo finish
 :end
